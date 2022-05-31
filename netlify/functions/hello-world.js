452027c65f508648cd1131e12b9d505bd01ac21f
Daniel Tschinder <231804+danez@users.noreply.github.com>
2022-05-31 11:32:18 +0200

    asdasd

--- netlify/functions/hello-world.js
+++ netlify/functions/hello-world.js
@@ -2,6 +2,6 @@ exports.handler = async () => {
   const mySecret = process.env.MY_SECRET;
   return {
     statusCode: 200,
-    body: `hello world! I have a ${mySecret}`,
+    body: `hello worddld! I have a ${mySecret}`,
   };
 };
