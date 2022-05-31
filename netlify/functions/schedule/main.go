b8e9b43908ea2eca10e4a509b1fb760d04a2c181
Daniel Tschinder <231804+danez@users.noreply.github.com>
2022-05-31 16:29:06 +0200

    goooooo

---
+++ netlify/functions/schedule/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+  "github.com/aws/aws-lambda-go/events"
+  "github.com/aws/aws-lambda-go/lambda"
+  "net/http"
+)
+
+func handler(request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+  fmt.Println("Hello World!")
+  return &events.APIGatewayProxyResponse{
+    StatusCode:        200,
+  }, nil
+}
+
+func main() {
+  // Make the handler available for Remote Procedure Call
+  lambda.Start(handler)
+}
