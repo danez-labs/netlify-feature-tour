2891a3030c980bc1b6b5306bfd59c4f38b69983d
Daniel Tschinder <231804+danez@users.noreply.github.com>
2022-06-02 11:42:31 +0200

    sdfghjghfgsdfg

--- netlify/edge-functions/admin.js
+++ netlify/edge-functions/admin.js
@@ -1,6 +1,20 @@
-export default (request, context) => {
-  const text = context.cookies.get("nf_jwt");
+import { GoTrueApi } from "https://deno.land/x/gotrue/mod.ts";
 
-  context.log(Deno.env.get("JWT_SECRET"));
-  return new Response(text);
+const api = new GoTrueApi({
+  url: "https://tschinder.space/.netlify/identity",
+});
+
+export default async (request, context) => {
+  const token = context.cookies.get("nf_jwt");
+  const user = token && (await api.getUser(token));
+
+  context.log(token, user);
+
+  if (!token || !user) {
+    return new Response("No", {
+      status: 401,
+      statusText: "Login required",
+    });
+  }
+  return new Response("Secret");
 };
