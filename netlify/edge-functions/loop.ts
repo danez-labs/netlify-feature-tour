969606a008dbbe778d0ebe5ea406761c7bfc60ae
Daniel Tschinder <231804+danez@users.noreply.github.com>
2023-03-08 16:06:18 +0100

    chore: asd

---
+++ netlify/edge-functions/loop.ts
@@ -0,0 +1,24 @@
+import type { Context } from "https://edge.netlify.com";
+
+export default async (req: Request, context: Context) => {
+  if (req.method === "POST") {
+    const formData = await req.formData();
+    const password = formData.get("password");
+    console.log("Comparing ", password);
+    const headers = new Headers({ Location: req.url });
+    if (password === Deno.env.get("ADMIN_PASSWORD")) {
+      headers.set(
+        "Set-Cookie",
+        "fedb_user=admin; Secure; HttpOnly; Max-Age=31536000"
+      );
+    }
+    return new Response(null, { status: 302, headers });
+  }
+
+  const d = context.cookies.get("fedb_user");
+  if (!d) {
+    return context.rewrite("/admin/password");
+  }
+};
+
+export const config = { path: ["/admin*"] };
