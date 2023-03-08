9e1c9f4ea2d83caab8643801e93356d2aaeef897
Daniel Tschinder <231804+danez@users.noreply.github.com>
2023-03-08 17:50:21 +0100

    Update astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
-export default {
-  buildOptions: {
-    sitemap: true,
-  },
-  renderers: ["@astrojs/renderer-preact"],
-};
+import preact from "@astrojs/preact";
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [preact(), sitemap()],
+});
