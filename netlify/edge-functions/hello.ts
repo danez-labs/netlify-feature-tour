ece57e09b1cf87b8b1e008428a709df5f09d48fa
Daniel Tschinder <231804+danez@users.noreply.github.com>
2023-04-11 17:29:01 +0200

    sdfsdfsdf

--- netlify/edge-functions/hello.ts
+++ netlify/edge-functions/hello.ts
@@ -1,4 +1,13 @@
 import { Config } from "https://edge.netlify.com";
+import { configAsync } from "https://deno.land/x/dotenv/mod.ts";
+
+try {
+  // when running locally, we need to call this
+  // it'll eerror in Netlify prod, but that's okay
+  await configAsync({ export: true });
+} catch (err) {
+  // ignore the error on netlify Prod
+}
 
 export default () => new Response("Hello world");
 
