efcfac285dc3aef23f22ea14b557b88fc3707e34
Daniel Tschinder <231804+danez@users.noreply.github.com>
2023-04-12 14:06:33 +0200

    asdsad

--- netlify/edge-functions/hello6.ts
+++ netlify/edge-functions/hello6.ts
@@ -1,6 +1,6 @@
 import { Config } from "https://edge.netlify.com";
 
-export default () => new Response("Hello world 2");
+export default () => new Response("Hello world 3");
 
 export const config: Config = {
   path: ["/hello6/*", "/hello6"],
