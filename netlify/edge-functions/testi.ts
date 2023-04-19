41b62c1796869d04a5a212d52cf6d80adff4b101
Daniel Tschinder <231804+danez@users.noreply.github.com>
2023-04-19 12:23:22 +0200

    sdfdfsgs

--- netlify/edge-functions/testi.ts
+++ netlify/edge-functions/testi.ts
@@ -4,6 +4,8 @@ export default async (request: Request, context: Context) => {
   const response = await context.next();
   const content = await response.text();
 
+  console.log("test");
+
   return new Response(String(content), response);
 };
 
